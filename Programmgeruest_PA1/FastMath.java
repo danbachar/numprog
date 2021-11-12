18d8221db77d5d51927c7117b0d28b9ccf334508
rskorobogat <rskorobogat@gmail.com>
2021-11-12 18:29:13 +0200

    created the fast inverse square root function

--- Programmgeruest_PA1/FastMath.java
+++ Programmgeruest_PA1/FastMath.java
@@ -10,16 +10,21 @@
  */
 public class FastMath {
 	/**
-	 * The "magic" constant which is used in the fast inverse square root
-	 * algorithm.
+	 * The "magic" constant which is used in the fast inverse square root algorithm.
 	 * 
 	 * The given initial value is just a test value for 8 mantissa bits and 4
 	 * exponent bits, and has to be optimized by the students.
 	 * 
-	 * In literature, several of those constants for floats or doubles can be
-	 * found. There's no optimal constant for all cases.
+	 * In literature, several of those constants for floats or doubles can be found.
+	 * There's no optimal constant for all cases.
 	 */
-	private static int MAGIC_NUMBER = 1024;
+
+	/*
+	The initial magic number was: 1024. Setting the magic number to 1000
+	*/
+	private static int MAGIC_NUMBER = 1024; // 8 mantissa bits and 4 exponent bits
+
+	
 
 	/**
 	 * belegt die MAGIC_NUMBER mit dem Wert magic
@@ -28,6 +33,9 @@ public class FastMath {
 		FastMath.MAGIC_NUMBER = magic;
 	}
 
+
+
+
 	/**
 	 * This method contains the code for the fast inverse square root algorithm
 	 * which can e.g. be found in "Fast Inverse Square Root" from Lomont, Chris
@@ -35,26 +43,44 @@ public class FastMath {
 	 * 
 	 * It approximately calculates the value 1 / sqrt(x).
 	 * 
-	 * No Newton steps to improve the result has to be implemented in this
-	 * exercise.
+	 * No Newton steps to improve the result has to be implemented in this exercise.
 	 * 
-	 * @param x
-	 *            Input value of which the inverse square root should be
-	 *            computed.
+	 * @param x Input value of which the inverse square root should be computed.
 	 * @return Approximation for 1 / sqrt(x).
 	 */
 	public static Gleitpunktzahl invSqrt(Gleitpunktzahl x) {
 
 		/* TODO: hier den "fast inverse square root" Algorithmus implementieren */
-		return new Gleitpunktzahl();
+		/*
+		 * Steps 1. Die Float-Zahl als einer 32-Bit int Zahl interpretieren
+		 */
+
+		//checking if the number is negative or 0
+		if(x.vorzeichen || x.isNull()){
+			x.setNaN();
+			return x;
+		}
+
+		// 1. Schritt
+		int res = gleitpunktzahlToIEEE(x);
+
+		// 2. Schritt
+		res /= 2;
+
+		// 3. Schritt
+		res = MAGIC_NUMBER - res;
+
+		// 4. Schritt
+		Gleitpunktzahl resultNumber = iEEEToGleitpunktzahl(res);
+
+		return resultNumber;
 	}
 
 	/**
-	 * Calculates the absolute error between the result of the fast inverse
-	 * square root algorithm and the "exact" IEEE-conform result.
+	 * Calculates the absolute error between the result of the fast inverse square
+	 * root algorithm and the "exact" IEEE-conform result.
 	 * 
-	 * @param x
-	 *            Position where the absolute error should be determined.
+	 * @param x Position where the absolute error should be determined.
 	 * @return Absolute error between invSqrt(x) and 1 / Math.sqrt(x).
 	 */
 	public static double absInvSqrtErr(Gleitpunktzahl x) {
@@ -66,11 +92,10 @@ public class FastMath {
 	}
 
 	/**
-	 * Calculates the relative error between the result of the fast inverse
-	 * square root algorithm and the "exact" IEEE-conform result.
+	 * Calculates the relative error between the result of the fast inverse square
+	 * root algorithm and the "exact" IEEE-conform result.
 	 * 
-	 * @param x
-	 *            Position where the relative error should be determined.
+	 * @param x Position where the relative error should be determined.
 	 * @return Relative error between invSqrt(x) and 1 / Math.sqrt(x).
 	 */
 	public static double relInvSqrtErr(Gleitpunktzahl x) {
@@ -83,25 +108,25 @@ public class FastMath {
 	/**
 	 * Uebersetzt die Gleitpunktzahl in eine Bitfolge (int) aehnlich dem IEEE
 	 * Standard, d.h. in die Form [Vorzeichen, Exponent, Mantisse], wobei die
-	 * führende 1 der Mantisse nicht gespeichert wird. Dieser Wechsel ist noetig
-	 * für ein Funktionieren des Fast Inverse Sqrt Algorithmus
+	 * führende 1 der Mantisse nicht gespeichert wird. Dieser Wechsel ist noetig für
+	 * ein Funktionieren des Fast Inverse Sqrt Algorithmus
 	 */
 	public static int gleitpunktzahlToIEEE(Gleitpunktzahl x) {
 		int sizeExponent = Gleitpunktzahl.getSizeExponent();
 		int sizeMantisse = Gleitpunktzahl.getSizeMantisse();
 
 		int result;
-		
+
 		/* mantisse ohne fuehrende 1 einfuegen */
-		int mask = (int) Math.pow(2, sizeMantisse-1) - 1;
+		int mask = (int) Math.pow(2, sizeMantisse - 1) - 1;
 		result = (x.mantisse & mask);
-		
+
 		/* exponent vorne anhaengen */
-		result |= (x.exponent << sizeMantisse-1);
-		
+		result |= (x.exponent << sizeMantisse - 1);
+
 		/* vorzeichen setzen */
 		if (x.vorzeichen)
-			result |= (1 << sizeExponent + sizeMantisse-1);
+			result |= (1 << sizeExponent + sizeMantisse - 1);
 
 		return result;
 	}
@@ -118,16 +143,16 @@ public class FastMath {
 
 		/* fuehrende 1 fuer mantisse eintragen */
 		g.mantisse = 1;
-		g.mantisse <<= sizeMantisse-1;
+		g.mantisse <<= sizeMantisse - 1;
 		/* mantisse ohne fuehrende 1 einfuegen */
-		int mask = (int) Math.pow(2, sizeMantisse-1) - 1;
+		int mask = (int) Math.pow(2, sizeMantisse - 1) - 1;
 		g.mantisse |= (b & mask);
 
 		/* exponent eintrage */
-		b >>= sizeMantisse-1;
+		b >>= sizeMantisse - 1;
 		mask = (int) Math.pow(2, sizeExponent) - 1;
 		g.exponent = (b & mask);
-		
+
 		/* vorzeichen setzen */
 		b >>= sizeExponent;
 		g.vorzeichen = (b & 1) > 0;
--- Programmgeruest_PA1/Test_FastInverse.java
+++ Programmgeruest_PA1/Test_FastInverse.java
@@ -13,22 +13,27 @@ import javax.swing.JFrame;
 public class Test_FastInverse {
 
 	/** Beispielwerte fuer IEEE Standard mit 32 Bits */
-	private static int MAGIC_NUMBER = 0x5f3759df;
-
+	private static int MAGIC_NUMBER = 0x5f3759df; //converted to decimal: 1 597 463 007 0b1011111001101110101100111011111
 	private static int anzBitsExponent = 8;
 	private static int anzBitsMantisse = 24;
 
 	/**
-	 * Uses the FastMath class and invokes the plotter. In a logarithmically
-	 * scaled system, the exakt solutions of 1/sqrt(x) are shown in green, and
-	 * the absolute errors of the Fast Inverse Square Root in red. Can be used
-	 * to test and debug the own implementation of the fast inverse square root
-	 * algorithm and to play while finding an optimal magic number.
+	 * Uses the FastMath class and invokes the plotter. In a logarithmically scaled
+	 * system, the exakt solutions of 1/sqrt(x) are shown in green, and the absolute
+	 * errors of the Fast Inverse Square Root in red. Can be used to test and debug
+	 * the own implementation of the fast inverse square root algorithm and to play
+	 * while finding an optimal magic number.
 	 * 
-	 * @param args
-	 *            args is ignored.
+	 * @param args args is ignored.
 	 */
 	public static void main(String[] args) {
+		// first trying out manually, setting the magic number to 1000
+		//
+		//FastMath.setMagic(1597463007);
+		Gleitpunktzahl.setSizeExponent(2); //changed from 4 to 2
+		Gleitpunktzahl.setSizeMantisse(8);
+		//FastMath.setMagic(0);
+		// findGoodMagicNumber()
 
 		Gleitpunktzahl.setSizeExponent(anzBitsExponent);
 		Gleitpunktzahl.setSizeMantisse(anzBitsMantisse);
@@ -60,5 +65,49 @@ public class Test_FastInverse {
 		frame.setSize(960, 720);
 		frame.setLocation(0, 0);
 		frame.setVisible(true);
+	}
+
+	private static void findGoodMagicNumber() {
+		int numberAbs = 1024;
+		int numberRel = 1024;
+		FastMath.setMagic(numberAbs);
+		Gleitpunktzahl.setSizeExponent(4);
+		Gleitpunktzahl.setSizeMantisse(8);
+		Gleitpunktzahl test = new Gleitpunktzahl(1000.0);
+		double min_abs_error = FastMath.absInvSqrtErr(test);
+		double min_rel_error = FastMath.relInvSqrtErr(test);
+		double cur_abs_err;
+		double cur_rel_err;
+		for (int step = 1; step < 502; step++) {
+			FastMath.setMagic(1024 + 2 * step);
+
+			cur_abs_err = FastMath.absInvSqrtErr(test);
+			cur_rel_err = FastMath.relInvSqrtErr(test);
+
+			if (cur_abs_err < min_abs_error) {
+				numberAbs = 1024 + 2 * step;
+				min_abs_error = cur_abs_err;
+			}
+			if (cur_rel_err < min_rel_error) {
+				numberRel = 1024 + 2 * step;
+				min_rel_error = cur_rel_err;
+			}
+		}
+		for (int step = 1; step < 502; step++) {
+			FastMath.setMagic(1024 - 2 * step);
+			cur_abs_err = FastMath.absInvSqrtErr(test);
+			cur_rel_err = FastMath.relInvSqrtErr(test);
+
+			if (cur_abs_err < min_abs_error) {
+				numberAbs = 1024 - 2 * step;
+				min_abs_error = cur_abs_err;
+			}
+			if (cur_rel_err < min_rel_error) {
+				numberRel = 1024 - 2 * step;
+				min_rel_error = cur_rel_err;
+			}
+		}
+		System.out.println("The smallest rel error is " + min_rel_error + ", the magic number is " + numberRel);
+		System.out.println("The smallest abs error is " + min_abs_error + ", the magic number is " + numberAbs);
 	}
 }
