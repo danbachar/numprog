28a2f5ae1e58dfee19ec24bc01af5161e41ac717
dbachar <dan.bachar@tum.de>
2021-11-26 19:07:01 +0100

    "wip on IFFT"

--- Programmgeruest_PA2/dft/IFFT.java
+++ Programmgeruest_PA2/dft/IFFT.java
@@ -1,6 +1,7 @@
 package dft;
 
 import java.util.Arrays;
+import java.util.stream.Stream;
 
 /**
  * Schnelle inverse Fourier-Transformation
@@ -15,7 +16,34 @@ public class IFFT {
      * Zweierpotenz ist. Es gilt also: c.length == 2^m fuer ein beliebiges m.
      */
     public static Complex[] ifft(Complex[] c) {
-        // TODO: diese Methode ist zu implementieren
-        return null;
+        int n = c.length;
+
+        if (n == 1) {
+            return c;
+        }
+        int m = n / 2;
+        Complex[] evens = new Complex[m];
+        Complex[] odds = new Complex[m];
+        for (int i=0; i<n; i++) {
+            if (i % 2 == 0) {
+                evens[i/2] = c[i];
+            } else {
+                odds[i/2] = c[i];
+            }
+        }
+        Complex[] z1 = ifft(evens);
+        Complex[] z2 = ifft(odds);
+
+        // double omega = Math.exp(2 * Math.PI / n); // TODO: add i, simplify using periodität
+        // TODO: make omega complex
+
+
+        Complex[] v = new Complex[n];
+        for (int j=0; j<m; j++) {
+            v[j]   = z1[j];// + Math.pow(omega, j) * z2[j];
+            v[m+j] = z1[j];// - Math.pow(omega, j) * z2[j];
+        }
+
+        return v;
     }
 }
