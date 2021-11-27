458812cc0ec957c87f3bd20e8f5b096ef0e7427a
Olha <olchik23012001@gmail.com>
2021-11-27 16:58:56 +0100

    Problems solved

--- Programmgeruest_PA2/Test_Interpolation.java
+++ Programmgeruest_PA2/Test_Interpolation.java
@@ -6,8 +6,10 @@ public class Test_Interpolation {
      * @param args
      */
     public static void main(String[] args) {
-        testNewton();
-        testSplines();
+        //testNewton();
+        //testSplines();
+        //testSplineOneUnknown();
+        testWithFive();
     }
 
     private static void testNewton() {
@@ -27,5 +29,20 @@ public class Test_Interpolation {
         spl.setBoundaryConditions(9, 0);
         System.out.println(Arrays.toString(spl.getDerivatives())
                 + " sollte sein: [9.0, -3.0, 3.0, 0.0].");
+    }
+
+    public static void testSplineOneUnknown(){
+        CubicSpline spl = new CubicSpline();
+        double[] y = { -3.0, 0.0, -1.0};
+        spl.init(-1, 1, 2, y);
+        spl.setBoundaryConditions(1, 2);
+        System.out.println(Arrays.toString(spl.getDerivatives()));
+    }
+
+    public static void testWithFive(){
+        CubicSpline spl = new CubicSpline();
+        double[] y = {1.0, -1.0, 0.3333333333333333, 7.0, 5.0};
+        spl.init(-1, 7, 4, y);
+        System.out.println(Arrays.toString(spl.getDerivatives()));
     }
 }
